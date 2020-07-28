b0e57760b3c405b1ab1219a4c9fba7f3b763bde8
Thomseeen <thwa1@web.de>
2020-07-28 14:29:20 +0200

    adapted simple python example

--- examples/simple-py-client/simple-c-client.py
+++ examples/simple-py-client/simple-c-client.py
@@ -1,13 +1,10 @@
 import struct
-import numpy as np
 import paho.mqtt.client as mqtt
 
 
 class MeasurementsRecoder:
 
-  def __init__(self, address, port, client_id, topic, qos, keep_alive, measurements_cnt):
-    self.measurements_indexer = 0
-    self.measurements = np.zeros((measurements_cnt, 2))
+  def __init__(self, address, port, client_id, topic, qos, keep_alive):
 
     self.client = mqtt.Client(client_id=client_id)
     self.client.on_connect = self.on_connect
@@ -33,14 +30,7 @@ class MeasurementsRecoder:
       seq_no = data[1]
       pin_no = data[2]
       status = data[3]
-      self.measurements[self.measurements_indexer][0] = seq_no
-      self.measurements[self.measurements_indexer][1] = value
-      self.measurements_indexer += 1
-
-      if self.measurements_indexer == MEASUREMENTS_CNT:
-        self.client.disconnect()
-        with open("measurements.npy", "wb") as file:
-          np.save(file, self.measurements)
+      print(f"Pin {pin_no} reads {value} at {seq_no}")
 
     except Exception:
       import traceback
@@ -51,12 +41,17 @@ if __name__ == "__main__":
   ADDRESS = "192.168.178.16"
   PORT = 1883
   CLIENTID = "mfd-py"
-  TOPIC = "lfd/#"
+  TOPIC = "lfd1/#"
   QOS = 0
   KEEP_ALIVE = 30  # s
-  MEASUREMENTS_CNT = 50000  # @100Hz with 1 pin -> 500s = 8.3min
 
-  measurements_recorder = MeasurementsRecoder(ADDRESS, PORT, CLIENTID, TOPIC, QOS, KEEP_ALIVE,
-                                              MEASUREMENTS_CNT)
+  measurements_recorder = MeasurementsRecoder(
+      ADDRESS,
+      PORT,
+      CLIENTID,
+      TOPIC,
+      QOS,
+      KEEP_ALIVE,
+  )
 
   measurements_recorder.start()
